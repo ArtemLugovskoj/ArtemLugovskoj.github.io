85790fcbf58988bccc149982ad49d42a02fa7489
Artem Lugovskoj <artemmaugli@gmail.com>
2024-04-24 01:27:27 +0300

    pr7 is done

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 var app = new Vue({
     el: "#products",
     data: {
-        products: [{ id: 1, title: "Kivi 1", short_text: 'Very sweet kiwi', image: '../img/kivi1.png', desc: "Kiwi is a round, green fruit that grows on tree-like vines. There is also a golden kiwi. China is considered to be the birthplace of the fruit. For this reason, kiwi is sometimes called Chinese gooseberry." },
-        { id: 2, title: "Kivi 2", short_text: 'Very good kiwi', image: '../img/kivi2.png', desc: "Kiwi is a round, green fruit that grows on tree-like vines. There is also a golden kiwi. China is considered to be the birthplace of the fruit. For this reason, kiwi is sometimes called Chinese gooseberry." },
-        { id: 3, title: "Kivi 3", short_text: 'Very green kiwi', image: '../img/kivi3.png', desc: "Kiwi is a round, green fruit that grows on tree-like vines. There is also a golden kiwi. China is considered to be the birthplace of the fruit. For this reason, kiwi is sometimes called Chinese gooseberry." },
-        { id: 4, title: "Kivi 4", short_text: 'Very beautiful kiwi', image: '../img/kivi4.png', desc: "Kiwi is a round, green fruit that grows on tree-like vines. There is also a golden kiwi. China is considered to be the birthplace of the fruit. For this reason, kiwi is sometimes called Chinese gooseberry."},
-        { id: 5, title: "Kivi 5", short_text: 'Very cool kiwi', image: '../img/kivi5.png', desc: "Kiwi is a round, green fruit that grows on tree-like vines. There is also a golden kiwi. China is considered to be the birthplace of the fruit. For this reason, kiwi is sometimes called Chinese gooseberry." }],
+        products: [{ id: 1, title: "Kivi 1", short_text: 'Very sweet kiwi', image: '../img/kivi1.png', desc: "Try our exotic kiwi fruit varieties - now available with free shipping." },
+        { id: 2, title: "Kivi 2", short_text: 'Very good kiwi', image: '../img/kivi2.png', desc: "Bulk discount: Save 30% when you buy five kilograms of kiwi fruit." },
+        { id: 3, title: "Kivi 3", short_text: 'Very green kiwi', image: '../img/kivi3.png', desc: "Get a free recipe book with every purchase of kiwi fruit." },
+        { id: 4, title: "Kivi 4", short_text: 'Very beautiful kiwi', image: '../img/kivi4.png', desc: "20% off on organic kiwi fruit for a limited time."},
+        { id: 5, title: "Kivi 5", short_text: 'Very cool kiwi', image: '../img/kivi5.png', desc: "Buy one dozen kiwi fruit, get one dozen free!" }],
         btnVisible: 0,
-        product: { }
-        
-    
+        product: { },
+        cart: [],
+        contactFields: [],
+        orderSubmitted: false
     },
     mounted: function () {
         console.log(window.localStorage.getItem('prod'));
         this.getProduct();
         this.checkInCart();
+        this.getCart();
     },
     
 
@@ -48,6 +50,35 @@ var app = new Vue({
         },
         checkInCart:function(){
             if(this.product && this.product.id && window.localStorage.getItem('cart').split(',').indexOf (String (this.product.id))!=-1) this.btnVisible=1;
-        }
+        },
+
+        getCart: function () {
+            var cartIds = [];
+            if (window.localStorage.getItem('cart')) {
+                cartIds = window.localStorage.getItem('cart').split(',');
+            }
+            for (i in this.products) {
+                if (this.products[i] && this.products[i].id && cartIds.includes(String(this.products[i].id) )) this.cart.push(this.products[i]);
+            }
+
+        },
+        removeFromCart: function (id) {
+            var cart = [];
+            if (window.localStorage.getItem('cart')) {
+                cart = window.localStorage.getItem('cart').split(',');
+            }
+            var index = cart.indexOf(String(id));
+            if (index > -1) {
+                cart.splice(index, 1);
+                window.localStorage.setItem('cart', cart.join());
+                this.cart = [];
+                this.getCart();
+            }
+        },
+        makeOrder: function () {
+            this.cart = [];
+            localStorage.removeItem('cart');
+            this.orderSubmitted = true;
+          }
     }
 });
